c4ca5e8f11f8e7899f8d30dc38adce3d593192f0
Felipe Bogaerts de Mattos <felipebogaerts@gmail.com>
2022-04-26 13:13:18 -0300

    0.1.3
    - Added gitignore
    - Config for static files
    - Fixed Docker image and entrypoint

--- web/scripts/entrypoint.sh
+++ web/scripts/entrypoint.sh
@@ -6,4 +6,4 @@ python3 manage.py collectstatic --noinput
 python3 manage.py migrate --noinput
 
 echo "STARTING GUNICORN SERVER..."
-gunicorn backend.wsgi:application -t 1800 --bind :8000
+gunicorn config.wsgi:application -t 1800 --bind :8000
